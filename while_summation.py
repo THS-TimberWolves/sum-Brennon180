dd4e3b23537e0c5247150de902c54bbdce499b81
Brennon180 <halesbre002@alpinesd.org>
2025-09-22 12:01:07 -0600

    Update while_summation.py

--- while_summation.py
+++ while_summation.py
@@ -1,14 +1,10 @@
+user_number = int(input())
 
-try:
-    user_number = int(input("Enter a number: "))
-except ValueError:
-    print("Invalid input. Please enter an integer.")
-    exit()
-
+    
 total_sum = 0
 
 for number in range(1, user_number + 1):
     total_sum += number
 
+print(total_sum)
 
-print(f"The sum of numbers from 1 to {user_number} is: {total_sum}")
